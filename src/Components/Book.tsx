74b92f2ef72af450fa4e2fdfb1897cac46146912
majazocom <maja.gedda@zocom.se>
2024-08-23 14:13:33 +0200

    draft done

--- src/Components/Book.tsx
+++ src/Components/Book.tsx
@@ -4,7 +4,7 @@ interface BookInterface {
     title: string;
     author: string;
     isbn: number;
-    genre?: Array<string>;
+    genres?: Array<string>;
     rating: number;
     cover?: string;
 }
@@ -12,9 +12,8 @@ function Book({ book, updateBook }: { book: BookInterface, updateBook: (updatedB
     const [edit, setEdit] = useState<boolean>(false);
     const [title, setTitle] = useState<string>(book.title);
     const [author, setAuthor] = useState<string>(book.author);
-    const [isbn, _] = useState<number>(book.isbn);
+    const [isbn] = useState<number>(book.isbn);
     const [rating, setRating] = useState<number>(book.rating);
-
     let stars = [];
     for (let i = 0; i < book.rating; i++) {
         stars.push(<span key={i}>⭐</span>)
@@ -25,32 +24,37 @@ function Book({ book, updateBook }: { book: BookInterface, updateBook: (updatedB
             title: title,
             author: author,
             isbn: isbn,
+            genres: book.genres,
             rating: rating,
+            cover: book.cover
         }
         updateBook(updatedBook);
         setEdit(false);
     };
 
     return (
-
         <article className="book-container">{edit ?
             <>
-                <figure style={{ backgroundImage: `url(${book.cover})` }}></figure>
-                <input defaultValue={title} onChange={(e) => setTitle(e.target.value)} />
-                <input defaultValue={author} onChange={(e) => setAuthor(e.target.value)} />
-                <p>{book.isbn}</p>
-                <p>Genre: {book.genre ? book.genre.join(", ") : "Not defined"}</p>
-                <input max="5" min="1" type='number' defaultValue={rating} onChange={(e) => setRating(parseInt(e.target.value))} />
+                <section className='book-info'>
+                    <figure style={{ backgroundImage: `url(${book.cover})` }}></figure>
+                    <input defaultValue={title} onChange={(e) => setTitle(e.target.value)} id='newTitle' />
+                    <input defaultValue={author} onChange={(e) => setAuthor(e.target.value)} id='newAuthor' />
+                    <p>{book.isbn}</p>
+                    <p>Genres: {book.genres ? book.genres.join(", ") : "Not defined"}</p>
+                    <input max="5" min="1" type='number' defaultValue={rating} onChange={(e) => setRating(parseInt(e.target.value))} id='newRating' />
+                </section>
                 <button onClick={handleEdit}>Confirm</button>
             </>
             :
             <>
-                <figure style={{ backgroundImage: `url(${book.cover})` }}></figure>
-                <h2>{book.title}</h2>
-                <p>{book.author}</p>
-                <p>{book.isbn}</p>
-                <p>Genre: {book.genre ? book.genre.join(", ") : "Not defined"}</p>
-                <p>{stars}</p>
+                <section className='book-info'>
+                    <figure style={{ backgroundImage: `url(${book.cover})` }}></figure>
+                    <h2>{book.title}</h2>
+                    <p>{book.author}</p>
+                    <p>{book.isbn}</p>
+                    <p>Genres: {book.genres ? book.genres.join(", ") : "Not defined"}</p>
+                    <p>{stars}</p>
+                </section>
                 <button onClick={() => { setEdit(true) }}>Edit</button>
             </>
         }
